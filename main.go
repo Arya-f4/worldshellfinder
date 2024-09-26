7910655f833c237837cb4482844b5b01d8464b29
Arya <kajuki27@gmail.com>
2024-09-26 15:34:53 +0700

    Update v.1.0.2.4

--- main.go
+++ main.go
@@ -13,18 +13,35 @@ import (
 	"time"
 )
 
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	White   = "\033[37m"
+)
+
 // Embed the default wordlist file
 //
 //go:embed wordlists/default.txt
 var defaultWordlist embed.FS
 
 const banner = `
+` + Red + `
+===========================================================================================
+` + Blue + `
  _    _            _     _ _____ _          _ _  ______ _           _           
 | |  | |          | |   | /  ___| |        | | | |  ___(_)         | |          
 | |  | | ___  _ __| | __| \ ` + "`" + `--.| |__   ___| | | | |_   _ _ __   __| | ___ _ __ 
 | |/\| |/ _ \| '__| |/ _` + "`" + ` |` + "`" + `--. \ '_ \ / _ \ | | |  _| | | '_ \ / _` + "`" + ` |/ _ \ '__|
 \  /\  / (_) | |  | | (_| /\__/ / | | |  __/ | | | |   | | | | | (_| |  __/ |   
- \/  \/ \___/|_|  |_|\__,_\____/|_| |_|\___|_|_| \_|   |_|_| |_|\__,_|\___|_|   
+ \/  \/ \___/|_|  |_|\__,_\____/|_| |_|\___|_|_| \_|   |_|_| |_|\__,_|\___|_|  
+ ` + Reset + `	
+ made with love by ` + Yellow + ` Worldsavior/Arya-f4 ` + Magenta + `^^	 ` + Green + `	v.1.0.2.3 Alpha Build  ` + Reset + `
+===========================================================================================
 `
 
 // Loading animation while scanning
@@ -107,7 +124,7 @@ func scanFiles(directory string, keywords []string, regexes []*regexp.Regexp) {
 				return nil // Continue scanning even if one file fails
 			}
 			if match {
-				fmt.Println("Potential webshell found in:", path)
+				fmt.Println("\n Potential webshell found in:", path)
 			}
 		}
 		return nil
